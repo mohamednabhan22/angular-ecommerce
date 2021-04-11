7c7fe462f24c2be9e17de1b0027653143badcdaa
mohamed <mohamednabhan2280@gmail.com>
2021-04-11 22:11:10 +0200

    update

--- backend/controllers/home.controller.js
+++ backend/controllers/home.controller.js
@@ -1,17 +1,16 @@
-const productsModel = require("../models/products.model");
+const productsModel = require('../models/products.model')
 
 exports.getHome = (req, res, next) => {
-   
-   productsModel.getAllProducts()
-    
-        .then(products => {
-            res.status(200).json({
-                products: products,
-               
-            });
-        })
-        .catch(err => {
-            let  error=validationResult(req).array()
-            res.status(400).send(error);
-        });
-};
+  productsModel
+    .getAllProducts()
+
+    .then((products) => {
+      res.status(200).json({
+        products: products,
+      })
+    })
+    .catch((err) => {
+      let error = validationResult(req).array()
+      res.status(400).send(error)
+    })
+}
